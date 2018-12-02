951178285232e7972cc4da2b8a80b3cd0d9928a9
xxhomey19 <xxhomey19@gmail.com>
2018-12-03 00:00:06 +1100

    Add index.d.ts

---
+++ inedx.d.ts
@@ -0,0 +1,15 @@
+interface Functions<T> {
+  set: (newPresent: T) => void;
+  undo: () => void;
+  redo: () => void;
+  canUndo: boolean;
+  canRedo: boolean;
+}
+
+interface State<T> {
+  past: T[];
+  present: T;
+  future: T[];
+}
+
+export default function useUndo<T>(initialPresent: T): [State<T>, Functions<T>];
